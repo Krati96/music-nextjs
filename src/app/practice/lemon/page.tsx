5c9b3ad8476dfd85b1da94070dbbea7a65804623
Krati96 <kv.krati.varshney@gmail.com>
2024-03-12 10:06:44 +0530

    Initial commit

---
+++ src/app/practice/lemon/page.tsx
@@ -0,0 +1,5 @@
+export default function lemonPage(){
+    return(
+        <h1>Lemon Tea</h1>
+    )
+}
---
+++ src/app/practice/page.tsx
@@ -0,0 +1,5 @@
+export default function practicePage(){
+    return(
+        <h1 className="text-3xl text-center mg-8">Hello Krati !</h1>
+    )
+}
