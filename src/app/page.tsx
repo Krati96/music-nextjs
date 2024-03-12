ef3c23eef4e02de2b0208ea18805d992f4ce6224
Krati96 <kv.krati.varshney@gmail.com>
2024-03-12 17:56:36 +0530

    add further section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 // import Image from "next/image";
 
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       {/* <h1 className="text-2xl text-center">Code</h1> */}
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
     </main>
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //   <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
